63da7823c4053a744b165475119b90dc3a1d1837
kparker028 <60025945+kparker028@users.noreply.github.com>
2020-08-28 20:14:34 -0600

    com

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import { Navbar } from "./components/navbar/navbar";
+import { Navbar } from './components/navbar/navbar';
 
 function App() {
   return (

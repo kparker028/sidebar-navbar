eedda45f76860065aca542cbd0d90fe67aeecf75
kparker028 <60025945+kparker028@users.noreply.github.com>
2020-08-28 19:21:18 -0600

    commit

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./navbar.css";
+
+export const Navbar = ({children}) => {
+    <div className="navContainer">
+        <div className="navNavigation">
+            <ol>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#"></a>About Us</li>
+                <li><a href="#"></a>Contact Us</li>
+            </ol>
+        
+        </div>
+    
+    
+    </div>
+}

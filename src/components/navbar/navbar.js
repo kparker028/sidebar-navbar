0e92b9d0f0b98f7e36df3ac8f6fef4332963f783
kparker028 <60025945+kparker028@users.noreply.github.com>
2020-09-01 19:40:11 -0600

    good

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 import "./navbar.css";
 import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 
-export const Navbar = ({children}) => {
+export const Navbar = ({}) => {
+    const [ sidebar, setSidebar ] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
     return(
     <div className="navbar">
         <Link to="#" className="menu-bars">
-            <FaIcons.FaBars />
+            <FaIcons.FaBars onClick={showSidebar}/>
         </Link>
     
     </div>
+    <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+            <li className="navbar-toggle">
+                <link to="#" className="menu-bars">
+                    <AiIcons.AiOutlineClose />
+                </link>
+            </li>
+        </ul>
+    </nav>
+
     );
 }

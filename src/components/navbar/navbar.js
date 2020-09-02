4df97c49ff5e4b3801185c2005a178b9d6e9b589
kparker028 <60025945+kparker028@users.noreply.github.com>
2020-09-01 20:33:09 -0600

    fin

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -13,18 +13,18 @@ function Navbar() {
         
         <div className="navbar">
             <Link to="#" className="menu-bars">
-                <FaIcons.FaBars onClick={showSidebar}/>
+                <FaIcons.FaBars onClick={showSidebar} />
             </Link>
     
         </div>
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <nav className={ sidebar ? "nav-menu active" : "nav-menu"}>
             <ul className="nav-menu-items">
                 <li className="navbar-toggle">
                     <link to="#" className="menu-bars">
                         <AiIcons.AiOutlineClose />
                     </link>
                 </li>
-                {SidebarData.map(( item, index ) => {
+                { SidebarData.map(( item, index ) => {
                     return(
                         <li key={index} className={item.cName}>
                             <link to={item.path}>

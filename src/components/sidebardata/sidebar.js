d992bd7ae50b58c2a568de622686654a2bdd8be1
kparker028 <60025945+kparker028@users.noreply.github.com>
2020-09-01 20:35:43 -0600

    good

--- src/components/sidebardata/sidebar.js
+++ src/components/sidebardata/sidebar.js
@@ -1,8 +1,8 @@
 import React from "react";
-import * as FaIcons from "react-icons/fa";
+
 import * as AiIcons from "react-icons/ai";
-import * as IoIcons from "react-icons/io";
-import { icons } from "react-icons/lib/cjs";
+
+
 
 export const SidebarData = [
     {

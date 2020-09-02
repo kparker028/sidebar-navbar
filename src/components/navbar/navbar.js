ec5449301dd414f65a043aa875ac62b500111d66
kparker028 <60025945+kparker028@users.noreply.github.com>
2020-09-01 20:20:58 -0600

    goood

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,38 +3,40 @@ import "./navbar.css";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
-import {SidebarData} from "./sidebardata";
+import { SidebarData } from "./sidebardata";
 
 function Navbar() {
     const [ sidebar, setSidebar ] = useState(false);
 
     const showSidebar = () => setSidebar(!sidebar);
     return(
-    <div className="navbar">
-        <Link to="#" className="menu-bars">
-            <FaIcons.FaBars onClick={showSidebar}/>
-        </Link>
+        
+        <div className="navbar">
+            <Link to="#" className="menu-bars">
+                <FaIcons.FaBars onClick={showSidebar}/>
+            </Link>
     
-    </div>
-    <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items">
-            <li className="navbar-toggle">
-                <link to="#" className="menu-bars">
-                    <AiIcons.AiOutlineClose />
-                </link>
-            </li>
-            {SidebarData.map((item,index)=>{
-                return(
-                    <li key={index} className={item.cName}>
-                        <link to={item.path}>
-                            {item.icon}
-                            <span>{item.title}</span>
-                        </link>
-                    </li>
-                );
-            })}
-        </ul>
-    </nav>
-
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items">
+                <li className="navbar-toggle">
+                    <link to="#" className="menu-bars">
+                        <AiIcons.AiOutlineClose />
+                    </link>
+                </li>
+                {SidebarData.map(( item, index ) => {
+                    return(
+                        <li key={index} className={item.cName}>
+                            <link to={item.path}>
+                                {item.icon}
+                                <span>{item.title}</span>
+                            </link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+              
     );
 }
+export default Navbar;
